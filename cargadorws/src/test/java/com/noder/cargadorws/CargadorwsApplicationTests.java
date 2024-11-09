e1b576cdbc03fa28072d3faa5427e07e41e8f846
Nodercif <gamunoz2022@udec.cl>
2024-11-09 18:30:54 -0300

    websocket inicial commit

---
+++ cargadorws/src/test/java/com/noder/cargadorws/CargadorwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noder.cargadorws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargadorwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
