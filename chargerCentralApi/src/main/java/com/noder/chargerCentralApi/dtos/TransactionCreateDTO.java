6a2fdeb9675a0fe34e6cf41750554d0fd2d3e7b1
Nodercif <gamunoz2022@udec.cl>
2024-12-11 20:59:42 -0300

    TransactionDTO

---
+++ chargerCentralApi/src/main/java/com/noder/chargerCentralApi/dtos/TransactionCreateDTO.java
@@ -0,0 +1,35 @@
+package com.noder.chargerCentralApi.dtos;
+
+import java.time.Instant;
+
+import com.noder.chargerCentralApi.models.Connector;
+
+public class TransactionCreateDTO {
+    private Connector connector;
+    private Instant start_time;
+    private String start_meter_value;
+
+    public void setConnectorId(Connector connector) {
+        this.connector = connector;
+    }
+
+    public void setStart_time(Instant start_time) {
+        this.start_time = start_time;
+    }
+
+    public void setStartMeter_value(String start_meter_value) {
+        this.start_meter_value = start_meter_value;
+    }
+
+    public Connector getConnector() {
+        return connector;
+    }
+
+    public Instant getStart_time() {
+        return start_time;
+    }
+
+    public String getStart_meter_value() {
+        return start_meter_value;
+    }
+}
