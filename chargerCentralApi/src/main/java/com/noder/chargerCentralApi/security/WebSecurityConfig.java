b1c1ce620c91d8e848bb4ea456d52e5d46e86ede
Sherlock898 <aguayo.sebastian.r@gmail.com>
2024-12-12 22:13:23 -0300

    feat: update WebSecurityConfig to use CustomUserDetailsService and implement authentication manager bean

--- chargerCentralApi/src/main/java/com/noder/chargerCentralApi/security/WebSecurityConfig.java
+++ chargerCentralApi/src/main/java/com/noder/chargerCentralApi/security/WebSecurityConfig.java
@@ -1,20 +1,22 @@
 package com.noder.chargerCentralApi.security;
 
+import org.apache.catalina.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    private final UserDetailsService userDetailsService;
+    private final CustomUserDetailsService userDetailsService;
 
-    public WebSecurityConfig(UserDetailsService userDetailsService) {
+    public WebSecurityConfig(CustomUserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
     }
 
@@ -22,13 +24,13 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
+            .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+            .httpBasic(Customizer.withDefaults());
         return http.build();
     }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return null;
-    }
     
+    @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
